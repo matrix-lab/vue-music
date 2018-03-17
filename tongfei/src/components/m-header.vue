065025bffe7fa9372e568ecdc5a5031e04942dde
tongfei <tf198910@163.com>
2018-03-17 15:39:10 +0800

    init demo

---
+++ tongfei/src/components/m-header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="text">Chicken Music</h1>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
