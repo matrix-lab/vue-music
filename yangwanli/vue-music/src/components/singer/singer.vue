762331b31ae8089460edfe4604303169cd463ad8
杨婉丽 <15538093733@163.com>
2018-03-19 15:43:44 +0800

    vue课程

---
+++ yangwanli/vue-music/src/components/singer/singer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="singer">
+    <list-view :data="singers"></list-view>
+  </div>
+</template>
+
+<script>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+  export default {
+    components:{
+      ListView
+    },
+    data () {
+      return {
+        singers: []
+      }
+    },
+    created () {
+      this._getSingerList()
+    },
+    methods:{
+      _getSingerList () {
+        getSingerList().then((res)=>{
+          if(ERR_OK === res.code){
+            this.singers = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      _normalizeSinger (list) {
+        let map={
+          hot:{
+            title: HOT_NAME,
+            items:[]
+          }
+        }
+        list.forEach((item,index)=>{
+          if(index<HOT_SINGER_LEN){
+            map.hot.items.push(new Singer({
+              id:item.Fsinger_mid,
+              name:item.Fsinger_name
+            }))
+          }
+          let key = item.Findex
+
+          if(!map[key]){
+            map[key] = {
+              title:key,
+              items:[]
+            }
+          }
+
+          map[key].items.push(new Singer({
+            id:item.Fsinger_mid,
+            name:item.Fsinger_name
+          }))
+        })
+        //列表有序化
+        let hot = []
+        let ret = []
+        for(let key in map){
+          let val = map[key]
+          if(val.title.match(/[a-zA-Z]/)){
+            ret.push(val)
+          }else if(val.title === HOT_NAME){
+            hot.push(val)
+          }
+        }
+        ret.sort((a,b)=>{
+          return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/custom"
+</style>
