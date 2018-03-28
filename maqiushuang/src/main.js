a3842cdc23f31fc899bebb69e6d0a6b1c5aa2137
maqiushuang <maqiushuang@1fangxin.cn>
2018-03-28 14:12:17 +0800

    轮播图

--- maqiushuang/src/main.js
+++ maqiushuang/src/main.js
@@ -6,9 +6,14 @@ import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
 import fastclick from 'fastclick'// 解决移动端点击300毫秒延迟
+import VueLazyLoad from 'vue-lazyload'
+
 fastclick.attach(document.body)
 
 // Vue.config.productionTip = false
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
