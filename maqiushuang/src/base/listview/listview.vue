a2e72c0a2615991443fe39306453c316146dd0bb
maqiushuang <maqiushuang@1fangxin.cn>
2018-04-12 15:06:46 +0800

    listview

--- maqiushuang/src/base/listview/listview.vue
+++ maqiushuang/src/base/listview/listview.vue
@@ -28,15 +28,27 @@
                 </li>
             </ul>
         </div>
+        <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+            <h1 class="fixed-title">{{fixedTitle}}</h1>
+        </div>
+        <div v-show="!data.length" class="loading-container">
+            <loading></loading>
+        </div>
     </scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   import {getData} from 'common/js/dom'
 
  const ANCHOR_HEIGHT = 18
-
+ const TITLE_HEIGHT = 30
+  //probeType:
+  // 1：会非实时（屏幕滑动超过一定时间后）派发scroll 事件。
+  // 2：会在屏幕滑动的过程中实时的派发 scroll 事件。
+  // 3：不仅在屏幕滑动的过程中，而且在 momentum 滚动动画运行过程中实时派发 scroll 事件。
+  // 如果没有设置该值，其默认值为 0，即不派发 scroll 事件
  export default {
    created() {
      this.touch = {}
@@ -47,7 +59,8 @@
    data() {
      return {
        scrollY: -1,
-       currentIndex: 0
+       currentIndex: 0,
+       diff: -1
      }
    },
    props: {
@@ -61,6 +74,12 @@
        return this.data.map((group) => {
          return group.title.substr(0,1)
        })
+     },
+     fixedTitle() {
+       if (this.scrollY >0 ){
+         return ''
+       }
+       return this.data[this.currentIndex] ?this.data[this.currentIndex].title : ''
      }
    },
    methods: {
@@ -73,7 +92,7 @@
      },
      onShortcutTouchMove(e) {
        let firstTouch = e.touches[0]
-       this.touch.y2  = firstTouch.pageY
+       this.touch.y2  = firstTouch.pageY9
        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
        let anchorIndex =  parseInt(this.touch.anchorIndex) + delta
        this._scrollTo(anchorIndex)
@@ -82,6 +101,15 @@
        this.scrollY = pos.y
      },
      _scrollTo(index) {
+       if(!index && index !==0) {
+          return
+       }
+       if(index < 0) {
+         index = 0
+       } else if (index > this.listHeight.length - 2) {
+         index = this.listHeight.length - 2
+       }
+       this.scrollY = -this.listHeight[index]
        this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0) // 第二个参数表示是否需要滚动动画，不需要时为0
      },
      _calculateHeight() {
@@ -104,19 +132,36 @@
      },
      scrollY(newY) {
        const listHeight = this.listHeight
+       //当滚动到顶部 ，newY>0
+       if(newY > 0) {
+         this.currentIndex = 0
+         return
+       }
+       //在中间部分滚动
        for (let i = 0; i < listHeight.length; i++) {
          let height1 = listHeight[i]
          let height2 = listHeight[i + 1]
-         if(!height2 || ( -newY > height1 && -newY < height2)){
+         if(!height2 || ( -newY >= height1 && -newY < height2)){
            this.currentIndex = i
+           this.diff = height2 + newY
            return
          }
        }
-       this.currentIndex = 0
+       //当滚动到底部，且-newY大于最后一个元素的上限
+       this.currentIndex = listHeight.length -2
+     },
+     diff(newVal) {
+       let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+       if(this.fixedTop === fixedTop) {
+         return
+       }
+       this.fixedTop = fixedTop
+       this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
      }
    },
    components: {
-     Scroll
+     Scroll,
+     Loading
    }
  }
 </script>
